19c34a73c1f788ee85a286ad381b16bade81dbf8
G-D4R3 <gdare1999@gmail.com>
2024-01-15 16:12:36 +0900

    [chapter7/FG-4] Chat Messages: component 익히기

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,28 @@
+import time
 import streamlit as st
 
-st.title("Document GPT")
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📃"
+)
+
+st.title("DocumentGPT")
+
+with st.chat_message("human"):
+    st.write("Helloooooo")
+
+
+with st.chat_message("ai"):
+    st.write("how are you")
+
+with st.status("Embedding file...", expanded=True) as status:
+    time.sleep(2)
+    st.write("Getting the file")
+    time.sleep(2)
+    st.write("Embedding the file")
+    time.sleep(2)
+    st.write("Caching the file")
+
+    status.update(label="Error", state="error")
+
+st.chat_input("Send an message to the ai")
