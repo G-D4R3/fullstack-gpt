f5ae20603338392866a1750ac3802c260dc6efd1
G-D4R3 <gdare1999@gmail.com>
2024-01-15 18:55:51 +0900

    [chapter7/FG-9] Streaming

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
@@ -52,21 +53,46 @@ def embed_file(file):
     return retriever
 
 
+def save_message(message, role):
+    st.session_state['messages'].append(dict(
+        message=message,
+        role=role
+    ))
+
+
 def send_message(message, role, save=True):
     with st.chat_message(role):
         st.markdown(message)
 
     if save:
-        st.session_state['messages'].append(dict(
-            message=message,
-            role=role
-        ))
+        save_message(message, role)
 
 
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
-llm = ChatOpenAI(temperature=0.1)
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ]
+)
 prompt = ChatPromptTemplate.from_messages([
     (
         "system",
@@ -104,10 +130,10 @@ if file:
         send_message(message, "human")
 
         chain = {
-            "context": retriever | RunnableLambda(format_docs),
-            "question": RunnablePassthrough()
-        } | prompt | llm
-        response = chain.invoke(message)
-        send_message(response.content, "ai")
+                    "context": retriever | RunnableLambda(format_docs),
+                    "question": RunnablePassthrough()
+                } | prompt | llm
+        with st.chat_message("ai"):
+            response = chain.invoke(message)
 else:
     st.session_state['messages'] = []
